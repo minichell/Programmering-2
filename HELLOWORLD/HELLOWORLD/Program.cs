ede9aa51730a5a6fa9e1cff496daf94a430da5c8
minichell <54796879+minichell@users.noreply.github.com>
2020-08-31 14:50:20 +0200

    Variabler och datatyper

---
+++ HELLOWORLD/HELLOWORLD/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HELLOWORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string yes;
+            
+            Console.WriteLine("What's your name? ");
+            name = Console.ReadLine();
+            Console.WriteLine("What's your age? ");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Are you alive?");
+            bool alive = true;
+            bool dead = false;
+            alive = bool.Parse(Console.ReadLine());
+            if (alive)
+            {
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Age: " + age);
+                Console.WriteLine("Alive: " + alive);
+            }
+            else
+            {
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Age: " + age);
+                Console.WriteLine("Alive: " + dead);
+            }
+          
+        }
+    }
+}
