d45bd0baea64afe3ffccd1dfd34c89d81504c4cb
minichell <54796879+minichell@users.noreply.github.com>
2020-09-02 22:29:09 +0200

    klar med villkor och loopar

--- HELLOWORLD/HELLOWORLD/Program.cs
+++ HELLOWORLD/HELLOWORLD/Program.cs
@@ -8,29 +8,63 @@ namespace HELLOWORLD
         {
             string name;
             int age;
-            string yes;
+            string aliveOrNot;
+           
             
             Console.WriteLine("What's your name? ");
             name = Console.ReadLine();
-            Console.WriteLine("What's your age? ");
+
+            Console.WriteLine("How old are you? ");
             age = int.Parse(Console.ReadLine());
+
+            for ( int i = 0; i < 1; i++)
+            {
+                if(age == 18)
+                {
+                    Console.WriteLine("cool");
+                }
+            }
+
             Console.WriteLine("Are you alive?");
             bool alive = true;
             bool dead = false;
-            alive = bool.Parse(Console.ReadLine());
-            if (alive)
+            aliveOrNot = Console.ReadLine();
+
+            if(aliveOrNot == "false")
             {
-                Console.WriteLine("Name: " + name);
-                Console.WriteLine("Age: " + age);
-                Console.WriteLine("Alive: " + alive);
-            }
-            else
-            {
+                Console.WriteLine("cool");
                 Console.WriteLine("Name: " + name);
                 Console.WriteLine("Age: " + age);
                 Console.WriteLine("Alive: " + dead);
+                alive = false;
             }
-          
+
+            while (alive)
+            {
+
+                Console.WriteLine("U wanna se the result?? ");
+                Console.WriteLine(" 1 = yes");
+                Console.WriteLine(" 2 = no");
+                int choosingResult = 1;
+                choosingResult = int.Parse(Console.ReadLine());
+                switch (choosingResult)
+                {
+                    case 1:
+                        Console.WriteLine("Name: " + name);
+                        Console.WriteLine("Age: " + age);
+                        Console.WriteLine("Alive: " + alive);
+       
+                       alive = false;
+                       break;
+
+                    case 2:
+
+                    Console.WriteLine("okayyy cyaaa");
+                        alive = false;
+                        break;
+                } 
+            }
+
         }
     }
 }
